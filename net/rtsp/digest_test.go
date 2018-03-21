6d993c754de4d84e354c43c349bcbc8d261d232d
Alexei Boukirev <aboukirev@gmail.com>
2018-03-21 12:01:40 -0500

    Adds simple tests for digest/basic authentication

---
+++ net/rtsp/digest_test.go
@@ -0,0 +1,29 @@
+package rtsp
+
+import (
+	"testing"
+)
+
+func TestDigestAuth(t *testing.T) {
+	digest, err := NewDigest("/dir/index.html", `Digest realm="testrealm@host.com", qop="auth", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41"`)
+	if err != nil {
+		t.Error(err)
+	}
+	authfunc := digest.Authenticate("Mufasa", "Circle Of Life")
+	// Expect something like.  cnonce is random so it's hard to predict
+	// Digest username="Mufasa", realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", uri="/dir/index.html", algorithm="MD5", opaque="5ccc069c403ebaf9f0171e9517f40e41", qop=auth, nc=00000001, cnonce="6443776e86b984dd", response="15f8e0d8b404b53a52e8cb7fa89988ee"
+	t.Log(authfunc("OPTIONS"))
+}
+
+func TestBasicAuth(t *testing.T) {
+	basic, err := NewDigest("/dir/index.html", `Basic realm="testrealm@host.com"`)
+	if err != nil {
+		t.Error(err)
+	}
+	authfunc := basic.Authenticate("Mufasa", "Circle Of Life")
+	// Expect: Basic TXVmYXNhOkNpcmNsZSBPZiBMaWZl
+	auth := authfunc("OPTIONS")
+	if auth != "Basic TXVmYXNhOkNpcmNsZSBPZiBMaWZl" {
+		t.Error(auth)
+	}
+}
